6548e9b47b820e8aabed831fe91f51519f761a51
LouiseCrowe <louise.crowe7@gmail.com>
2020-12-13 22:50:50 +0000

    Search field navigating successfully to ShowDetails Page

--- Show.cs
+++ Show.cs
@@ -4,30 +4,51 @@ using System.Text;
 
 namespace MyShowWatch.Shared
 {
-   
+
     public class Show
     {
-        public string Title { get; set; }
+        public string Title { get; set; }                   // show title
 
-        public ShowType ShowType { get; set; }
+        public ShowType ShowType { get; set; }              // movie, tv show, documentary
 
-        public Status Status { get; set; }
+        public Status Status { get; set; }                  // show ongoing indicates 
+                                                            // indicates if or when it will 
+                                                            // be available
 
-        public bool IsWatched { get; set; }
+        public bool IsWatched { get; set; }                 // have you watched the show yet?
 
-        public DateTime? ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }          // scheduled release date, may be unknown (null)
 
-        public bool IsKidFriendly { get; set; }
-              
-        public bool SeriesComplete { get; set; }
+        public bool ShowComplete { get; set; }              // has the show run finished
+                                                            // are there any future releases planned
+        public int? NumSeasonsConfirmed { get; set; }       // number of seasons confirmed
+                                                            // may or may not be available to view yet
 
-        public int? NumSeasonsConfirmed { get; set; }
+        public int? LatestSeasonAvailable { get; set; }     // number of latest season available
 
-        public int? LatestSeasonAvailable { get; set; }
+        public int? NumEpisodes { get; set; }               // number of episodes in latest season
 
-        public int? NumEpisodes { get; set; }
+        public int? NumMovies { get; set; }                 // number of movies in series
 
-        public string Description { get; set; }
-       
+        public int? LatestMovieAvailable { get; set; }      // number of latest movie available
+
+        public bool IsKidFriendly { get; set; }             // suitable for children
+
+        public string Description { get; set; }             // show description                
+    }
+}
+
+namespace MyShowWatch.Shared
+{
+    static class Test
+    {
+        static void Main()
+        {
+            Show s1 = new Show();
+            
+            s1.ReleaseDate = new DateTime(2020, 12, 25);
+
+            Console.WriteLine(s1.ReleaseDate.Value.ToShortDateString());
+        }
     }
 }
